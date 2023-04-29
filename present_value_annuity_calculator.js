48f43c3581c25619b1cba31fcebe621c67ef0894
Sean T. Ward <130578074+WardSean@users.noreply.github.com>
2023-04-28 21:23:24 -0400

    Add files via upload

--- present_value_annuity_calculator.js
+++ present_value_annuity_calculator.js
@@ -1,19 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const presentValueForm = document.getElementById("present-value-form");
-  
-    presentValueForm.addEventListener("submit", function(event) {
-      event.preventDefault();
-  
-      const paymentAmount = parseFloat(document.getElementById("paymentAmount").value);
-      const interestRate = parseFloat(document.getElementById("interestRate").value) / 100 / 12;
-      const numPayments = parseInt(document.getElementById("numPayments").value);
-      
-      const presentValue = (paymentAmount * (1 - Math.pow(1 + interestRate, -numPayments))) / interestRate;
-  
-      const presentValueRounded = Math.round(presentValue * 100) / 100;
-      
-      document.getElementById("presentValueResult").innerHTML = "The present value is $" + presentValue.toFixed(2);
-    });
-  });
-  
-  
+// Present Value of Annuity Calculator
+function presentValueAnnuityCalculator() {
+  // Get input values
+  const paymentAmount = Number(document.getElementById('paymentAmount').value);
+  const interestRate = Number(document.getElementById('interestRate').value);
+  const numPayments = Number(document.getElementById('numPayments').value);
+
+  // Calculate present value of annuity
+  const presentValue = paymentAmount * ((1 - Math.pow((1 + interestRate / 1200), -numPayments)) / (interestRate / 1200));
+
+  // Display result
+  document.getElementById('presentValueResult').innerHTML = `<p>The present value of the annuity is $${presentValue.toFixed(2)}</p>`;
+}
+
+// Attach event listener to form submit button
+document.querySelector('form').addEventListener('submit', function (event) {
+  event.preventDefault();
+  presentValueAnnuityCalculator();
+});
--- script.js
+++ script.js
@@ -1,55 +1,81 @@
 // Mortgage Calculator
-document.addEventListener("DOMContentLoaded", function() {
-    const mortgageForm = document.querySelector("#mortgageForm");
-    mortgageForm.addEventListener("submit", function(event) {
-      event.preventDefault();
-  
-      const principal = document.querySelector("#principal").value;
-      const interestRate = document.querySelector("#interestRate").value / 100 / 12;
-      const payments = document.querySelector("#payments").value;
-  
-      const x = Math.pow(1 + interestRate, payments);
-      const monthly = (principal * x * interestRate) / (x - 1);
-  
-      const result = document.querySelector("#result");
-      result.innerHTML = `$${monthly.toFixed(2)} per month`;
-    });
-  });
-  
-  // Future Value Calculator
-  document.addEventListener("DOMContentLoaded", function() {
-    const futureValueForm = document.querySelector("#futureValueForm");
-    futureValueForm.addEventListener("submit", function(event) {
-      event.preventDefault();
-  
-      const presentValue = document.querySelector("#presentValue").value;
-      const interestRate = document.querySelector("#interestRate").value / 100;
-      const years = document.querySelector("#years").value;
-  
-      const futureValue = presentValue * Math.pow(1 + interestRate, years);
-  
-      const result = document.querySelector("#result");
-      result.innerHTML = `$${futureValue.toFixed(2)}`;
-    });
-  });
-  
-  // Present Value of Annuity Calculator
-  document.addEventListener("DOMContentLoaded", function() {
-    const presentValueForm = document.querySelector("#presentValueForm");
-    presentValueForm.addEventListener("submit", function(event) {
-      event.preventDefault();
-  
-      const paymentAmount = document.querySelector("#paymentAmount").value;
-      const interestRate = document.querySelector("#interestRate").value / 100 / 12;
-      const numPayments = document.querySelector("#numPayments").value;
-  
-      const presentValue =
-        (paymentAmount *
-          (1 - Math.pow(1 + interestRate, -numPayments))) /
-        interestRate;
-  
-      const result = document.querySelector("#result");
-      result.innerHTML = `$${presentValue.toFixed(2)}`;
-    });
-  });
-  
+
+function calculateMortgage() {
+  const principal = parseFloat(document.getElementById("principal").value);
+  const interestRate = parseFloat(document.getElementById("interestRate").value) / 100 / 12;
+  const numPayments = parseFloat(document.getElementById("numPayments").value);
+
+  const monthlyPayment = (principal * interestRate * Math.pow(1 + interestRate, numPayments)) / (Math.pow(1 + interestRate, numPayments) - 1);
+
+  const monthlyPaymentFormatted = monthlyPayment.toFixed(2);
+  const totalPayments = (monthlyPayment * numPayments).toFixed(2);
+  const totalInterest = (totalPayments - principal).toFixed(2);
+
+  const resultElement = document.getElementById("mortgageResult");
+  resultElement.innerHTML = `<p>Your monthly payment is: $${monthlyPaymentFormatted}</p>
+    <p>You will pay a total of $${totalPayments}, of which $${totalInterest} is interest.</p>`;
+}
+
+const mortgageForm = document.getElementById('mortgage-form');
+const resultDiv = document.getElementById('result');
+
+mortgageForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const principal = parseFloat(document.getElementById('principal').value);
+  const interestRate = parseFloat(document.getElementById('interest-rate').value) / 100 / 12;
+  const loanLength = parseFloat(document.getElementById('loan-length').value) * 12;
+
+  const monthlyPayment = (principal * interestRate * Math.pow(1 + interestRate, loanLength)) / (Math.pow(1 + interestRate, loanLength) - 1);
+  const totalInterest = monthlyPayment * loanLength - principal;
+
+  resultDiv.innerHTML = `
+    <p>Monthly payment: $${monthlyPayment.toFixed(2)}</p>
+    <p>Total interest paid: $${totalInterest.toFixed(2)}</p>
+  `;
+});
+
+// Present Value Annuity Calculator
+
+function calculatePresentValue() {
+  const paymentAmount = parseFloat(document.getElementById("paymentAmount").value);
+  const interestRate = parseFloat(document.getElementById("interestRate").value) / 100 / 12;
+  const numPayments = parseFloat(document.getElementById("numPayments").value);
+
+  const presentValue = paymentAmount * ((1 - Math.pow(1 + interestRate, -numPayments)) / interestRate);
+
+  const presentValueFormatted = presentValue.toFixed(2);
+
+  const resultElement = document.getElementById("presentValueResult");
+  resultElement.innerHTML = `<p>The present value of your annuity is: $${presentValueFormatted}</p>`;
+}
+
+const presentValueForm = document.getElementById("presentValueForm");
+presentValueForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  calculatePresentValue();
+});
+
+// Future Value Calculator
+
+function calculateFutureValue() {
+  const presentValue = parseFloat(document.getElementById("deposit").value);
+  const annualInterestRate = parseFloat(document.getElementById("interest").value);
+  const years = parseFloat(document.getElementById("years").value);
+
+  const interestRatePerDay = (annualInterestRate / 100) / 365;
+  const numPeriods = years * 365;
+  const futureValue = presentValue * Math.pow(1 + interestRatePerDay, numPeriods);
+
+  const futureValueFormatted = futureValue.toFixed(2);
+
+  const resultElement = document.getElementById("futureValueResult");
+  resultElement.innerHTML = `<p>The future value of your investment is: $${futureValueFormatted}</p>`;
+}
+
+
+const futureValueForm = document.getElementById("futureValueForm");
+futureValueForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  calculateFutureValue();
+});
